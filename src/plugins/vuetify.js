f6d1f79267822dd35ff53a72a8562f491cf2714b
森田順也 <mjunya@me.com>
2020-09-08 00:27:02 +0900

    余計なコードを削除

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,28 +2,24 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify, {
-    theme: {
-        primary: '#CE3772',
-        secondary: '#3F557F',
-        accent: '#CFC34C',
-        error: '#b71c1c', 
-        text_main:'#212121',
-        text_sub: '#767676',
-        background:'#BDC4D2'
-    },
-})
+Vue.use(Vuetify);
 
-const opts = {
-    theme: {
-        primary: '#CE3772',
-        secondary: '#3F557F',
-        accent: '#CFC34C',
-        error: '#b71c1c', 
-        text_main:'#212121',
-        text_sub: '#767676',
-        background:'#BDC4D2'
+export default new Vuetify({
+    icons: {
+      iconfont: 'mdi'
     },
-}
-
-export default new Vuetify()
+    theme: {
+        themes: {
+            light: {
+                primary: '#CE3772',
+                secondary: '#3F557F',
+                accent: '#CFC34C',
+                error: '#b71c1c', 
+                text_main:'#212121',
+                text_sub: '#767676',
+                background:'#BDC4D2'
+            },
+        },
+    },
+    customVariables: ['~/sass/variables.scss'],
+});
